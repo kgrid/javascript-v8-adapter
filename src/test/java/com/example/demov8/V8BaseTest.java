b6792c572dcf0e351b362007a2145a867fe01512
Peter Boisvert <pboisver@umich.edu>
2020-07-10 17:43:53 -0400

    Got polyglot bindings working! See polyglotBindingTest()

--- src/test/java/com/example/demov8/V8BaseTest.java
+++ src/test/java/com/example/demov8/V8BaseTest.java
@@ -2,17 +2,15 @@ package com.example.demov8;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.Engine;
-import org.graalvm.polyglot.Source;
-import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.proxy.ProxyObject;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
-import java.lang.reflect.Executable;
 import java.util.Collections;
 import java.util.Map;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.PolyglotAccess;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+import org.junit.jupiter.api.Test;
 
 public class V8BaseTest {
 
@@ -41,8 +39,8 @@ public class V8BaseTest {
   public void initializeWithBinding() throws IOException {
     Context context = Context.create("js");
     String jsCode = "function doStuff(p1, p2) {" +
-            "return p1 + p2 + thing;" +
-            "}";
+        "return p1 + p2 + thing;" +
+        "}";
     context.getBindings("js").putMember("thing", 543);
 
     Source sourceCode = Source.newBuilder("js", jsCode, "jsCode.js").build();
@@ -55,8 +53,9 @@ public class V8BaseTest {
   @Test
   public void addJavaObjectToBinding() throws IOException {
     Map valueStore = Collections.singletonMap("key",998);
-    Context context = Context.newBuilder("js").allowHostAccess(false).build();
-    String jsCode = "function doStuff(p1, p2) {" +
+    Context context = Context.newBuilder().allowHostAccess(HostAccess.ALL).build();
+    String jsCode =
+        "function doStuff(p1, p2) {" +
             "console.log(valueStore.get(\"key\"));" +
             "}";
     context.getBindings("js").putMember("valueStore", valueStore);
@@ -65,6 +64,40 @@ public class V8BaseTest {
     context.eval(sourceCode);
     Value function = context.getBindings("js").getMember("doStuff");
     Value result = function.execute(2, 5);
+  }
+
+  @Test
+  public void polyglotBindingTest() throws IOException {
+    Map valueStore = Collections.singletonMap("key",998);
+    Context context = Context.newBuilder()
+        .allowPolyglotAccess(PolyglotAccess.ALL)
+        .allowHostAccess(HostAccess.ALL)
+        .build();
+    String jsCode =
+        "function doStuff(p1, p2) {" +
+            "var v = Polyglot.import(\"valueStore\");" +
+            "console.log(v.get(\"key\"));" +
+            "return p1 + p2 + v.get(\"key\");" +
+            "}";
+    context.getPolyglotBindings().putMember("valueStore", valueStore);
+
+    final Source js = Source.newBuilder("js", jsCode, "jsCode.js")
+        .build();
+    context.eval(js);
+
+    Value function = context.getBindings("js").getMember("doStuff");
+    assertEquals(1004, function.execute(3, 3).asInt());
+
+
+    Context context2 = Context.newBuilder()
+        .allowPolyglotAccess(
+            PolyglotAccess.newBuilder().allowBindingsAccess("js").build())
+        .build();
+
+    context.eval(js);
+    Value f2 = context.getBindings("js").getMember("doStuff");
+
+    assertEquals(1006, f2.execute(4, 4).asInt());
   }
 }
 
