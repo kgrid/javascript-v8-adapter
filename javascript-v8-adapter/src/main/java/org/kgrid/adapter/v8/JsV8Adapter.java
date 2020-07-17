a78d5020909d1339e205f8e7f8a52ea2f25d9cb0
mradamgbeck <mradamgbeck@gmail.com>
2020-07-17 15:43:56 -0400

    changed adapter type to V8

--- javascript-v8-adapter/src/main/java/org/kgrid/adapter/v8/JsV8Adapter.java
+++ javascript-v8-adapter/src/main/java/org/kgrid/adapter/v8/JsV8Adapter.java
@@ -1,8 +1,10 @@
 package org.kgrid.adapter.v8;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import java.util.function.Function;
-import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Engine;
+import org.graalvm.polyglot.HostAccess;
+import org.graalvm.polyglot.Value;
 import org.kgrid.adapter.api.ActivationContext;
 import org.kgrid.adapter.api.Adapter;
 import org.kgrid.adapter.api.AdapterException;
@@ -13,93 +15,72 @@ import java.nio.file.Paths;
 
 public class JsV8Adapter implements Adapter {
 
-  Engine engine;
-  ActivationContext activationContext;
+    Engine engine;
+    ActivationContext activationContext;
 
-  @Override
-  public String getType() {
-    return "JAVASCRIPT";
-  }
-
-  @Override
-  public void initialize(ActivationContext context) {
-
-    this.activationContext = context;
-    engine = Engine.newBuilder().build();
-  }
-
-  @Override
-  @Deprecated
-  public Executor activate(Path resource, String entry) {
-    return null;
-  }
-
-  @Override
-  public Executor activate(
-      String objectLocation, String arkId, String endpointName, JsonNode deploymentSpec) {
-
-    // Might need to wrap in try with resources to have context close on failure
-    Context context =
-        Context.newBuilder("js")
-            .allowHostAccess(HostAccess.ALL)
-            .allowExperimentalOptions(true)
-            .option("js.experimental-foreign-object-prototype", "true")
-            .allowHostClassLookup(className -> true)
-            .allowNativeAccess(true)
-            .build();
-    Value function;
-
-    String artifact = deploymentSpec.get("artifact").asText();
-    String artifactLocation = Paths.get(objectLocation, artifact).toString();
-
-    try {
-      context.getBindings("js").putMember("context", activationContext);
-
-      // Create the base function from our artifact(s)
-      byte[] src = activationContext.getBinary(artifactLocation);
-      context.eval("js", new String(src));
-      String functionName = deploymentSpec.get("function").asText();
-      function = context.getBindings("js").getMember(functionName);
-
-    } catch (Exception e) {
-      throw new AdapterException("Error loading source", e);
+    @Override
+    public String getType() {
+        return "V8";
     }
 
-    return new Executor() {
-      @Override
-      public Object execute(Object input) {
+    @Override
+    public void initialize(ActivationContext context) {
+
+        activationContext = context;
+        engine = Engine.newBuilder().build();
+    }
+
+    @Override
+    @Deprecated
+    public Executor activate(Path resource, String entry) {
+        return null;
+    }
+
+    @Override
+    public Executor activate(
+            String objectLocation, String arkId, String endpointName, JsonNode deploymentSpec) {
+
+        Context context =
+                Context.newBuilder("js")
+                        .allowHostAccess(HostAccess.ALL)
+                        .allowExperimentalOptions(true)
+                        .option("js.experimental-foreign-object-prototype", "true")
+                        .allowHostClassLookup(className -> true)
+                        .allowNativeAccess(true)
+                        .build();
+        String artifact = deploymentSpec.get("artifact").asText();
+        String artifactLocation = Paths.get(objectLocation, artifact).toString();
+        String functionName;
         try {
-          Value result = getWrapper(context).execute(function,input);
-          return result.as(Object.class);
-        } catch (PolyglotException e) {
-          throw new AdapterException("Code execution error", e);
+            context.getBindings("js").putMember("context", activationContext);
+            byte[] src = activationContext.getBinary(artifactLocation);
+            functionName = deploymentSpec.get("function").asText();
+            context.eval("js", new String(src));
+        } catch (Exception e) {
+            throw new AdapterException("Error loading source", e);
         }
-      }
-    };
-  }
-
-  private Value getWrapper(Context context) {
-    Value wrapper;// Put a helper function in the Context
-    context.eval("js",
-        "function wrapper(baseFunction, arg) { "
-            + "/* console.log(baseFunction.name); */"
-            + "let parsedArg;"
-            + "try {"
-            + "   parsedArg = JSON.parse(arg);"
-            + "} catch (error) {"
-            + "   return baseFunction(arg);"
-            + "}"
-            + "return baseFunction(parsedArg);"
-            + "}");
-    wrapper = context.getBindings("js").getMember("wrapper");
-    return wrapper;
-  }
-
-  @Override
-  public String status() {
-    if (engine == null) {
-      return "DOWN";
+        return new V8Executor(createWrapperFunction(context), context.getBindings("js").getMember(functionName));
     }
-    return "UP";
-  }
+
+    @Override
+    public String status() {
+        if (engine == null) {
+            return "DOWN";
+        }
+        return "UP";
+    }
+
+    private Value createWrapperFunction(Context context) {
+        context.eval("js",
+                "function wrapper(baseFunction, arg) { "
+                        + "let parsedArg;"
+                        + "try {"
+                        + "   parsedArg = JSON.parse(arg);"
+                        + "} catch (error) {"
+                        + "   return baseFunction(arg);"
+                        + "}"
+                        + "return baseFunction(parsedArg);"
+                        + "}");
+        return context.getBindings("js").getMember("wrapper");
+    }
 }
--- javascript-v8-adapter/src/main/java/org/kgrid/adapter/v8/V8Executor.java
+++ javascript-v8-adapter/src/main/java/org/kgrid/adapter/v8/V8Executor.java
@@ -1,4 +1,27 @@
 package org.kgrid.adapter.v8;
 
-public class V8Executor {
+import org.graalvm.polyglot.Value;
+import org.kgrid.adapter.api.AdapterException;
+import org.kgrid.adapter.api.Executor;
+
+public class V8Executor implements Executor {
+
+    private Value wrapper;
+    private Value function;
+
+    public V8Executor(Value wrapper, Value function) {
+        this.wrapper = wrapper;
+        this.function = function;
+    }
+
+    @Override
+    public Object execute(Object input) {
+        try {
+            Value result = wrapper.execute(function, input);
+            return result.as(Object.class);
+        } catch (Exception e) {
+            throw new AdapterException("Code execution error", e);
+        }
+    }
+
 }
