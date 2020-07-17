477ed5f647a5b679afc14cf051a059b773a84b5a
mradamgbeck <mradamgbeck@gmail.com>
2020-07-17 12:58:47 -0400

    renamed test

--- javascript-v8-adapter/src/test/java/org/kgrid/adapter/v8/JsV8ExampleKOTest.java
+++ javascript-v8-adapter/src/test/java/org/kgrid/adapter/v8/JsV8ExampleKOTest.java
@@ -14,54 +14,54 @@ import static org.junit.Assert.assertEquals;
 
 public class JsV8ExampleKOTest {
 
-  Adapter adapter;
-  TestActivationContext activationContext;
+    Adapter adapter;
+    TestActivationContext activationContext;
 
-  @Before
-  public void setUp() {
-    activationContext = new TestActivationContext();
-    adapter = new JsV8Adapter();
-    adapter.initialize(activationContext);
-  }
+    @Before
+    public void setUp() {
+        activationContext = new TestActivationContext();
+        adapter = new JsV8Adapter();
+        adapter.initialize(activationContext);
+    }
 
-  private JsonNode getDeploymentSpec(String deploymentLocation) throws IOException {
-    YAMLMapper yamlMapper = new YAMLMapper();
-    ClassPathResource classPathResource = new ClassPathResource(deploymentLocation);
-    JsonNode deploymentSpec =
-        yamlMapper.readTree(classPathResource.getInputStream().readAllBytes());
-    return deploymentSpec;
-  }
+    @Test
+    public void testSampleSimpleObject() throws IOException {
+        JsonNode deploymentSpec = getDeploymentSpec("v8-bmicalc-v1.0/deployment.yaml");
+        JsonNode endpointObject = deploymentSpec.get("endpoints").get("/bmicalc");
+        Executor executor = adapter.activate("v8-bmicalc-v1.0", "", "", endpointObject);
+        Object bmiResult = executor.execute("{\"weight\":70, \"height\":1.70}");
+        assertEquals("24.2", bmiResult);
+    }
 
+    @Test
+    public void testSampleExecutiveObjectWithStringifiedInputObjects() throws IOException {
+        JsonNode deploymentSpec = getDeploymentSpec("hello-world/deploymentSpec.yaml");
+        JsonNode endpointObject = deploymentSpec.get("endpoints").get("/welcome");
+        Executor helloExecutor = adapter.activate("hello-world", "", "", endpointObject);
+        activationContext.addExecutor("hello-world/welcome", helloExecutor);
 
-  @Test
-  public void testSampleSimpleObject() throws IOException {
-    JsonNode deploymentSpec = getDeploymentSpec("v8-bmicalc-v1.0/deployment.yaml");
-    JsonNode endpointObject = deploymentSpec.get("endpoints").get("/bmicalc");
-    Executor executor = adapter.activate("v8-bmicalc-v1.0", "", "", endpointObject);
-    Object bmiResult = executor.execute("{\"weight\":70, \"height\":1.70}");
-    assertEquals("24.2", bmiResult);
-  }
+        deploymentSpec = getDeploymentSpec("v8-bmicalc-v1.0/deployment.yaml");
+        endpointObject = deploymentSpec.get("endpoints").get("/bmicalc");
+        helloExecutor = adapter.activate("v8-bmicalc-v1.0", "", "", endpointObject);
+        activationContext.addExecutor("v8-bmicalc-v1.0/bmicalc", helloExecutor);
 
-  @Test
-  public void testSampleExecutiveObject() throws IOException {
-    JsonNode deploymentSpec = getDeploymentSpec("hello-world/deploymentSpec.yaml");
-    JsonNode endpointObject = deploymentSpec.get("endpoints").get("/welcome");
-    Executor helloExecutor = adapter.activate("hello-world", "", "", endpointObject);
-    activationContext.addExecutor("hello-world/welcome", helloExecutor);
+        deploymentSpec = getDeploymentSpec("v8-executive-1.0.0/deployment.yaml");
+        endpointObject = deploymentSpec.get("endpoints").get("/process");
+        Executor executor = adapter.activate("v8-executive-1.0.0", "", "", endpointObject);
+        Object helloResult = executor.execute("{\"name\":\"Bob\", \"weight\":70, \"height\":1.70}");
+        assertEquals("{message: \"Hello, Bob\", bmi: \"24.2\"}",
+                helloResult.toString()
+        );
+    }
 
-     deploymentSpec = getDeploymentSpec("v8-bmicalc-v1.0/deployment.yaml");
-     endpointObject = deploymentSpec.get("endpoints").get("/bmicalc");
-     helloExecutor = adapter.activate("v8-bmicalc-v1.0", "", "", endpointObject);
-    activationContext.addExecutor("v8-bmicalc-v1.0/bmicalc", helloExecutor);
+    private JsonNode getDeploymentSpec(String deploymentLocation) throws IOException {
+        YAMLMapper yamlMapper = new YAMLMapper();
+        ClassPathResource classPathResource = new ClassPathResource(deploymentLocation);
+        JsonNode deploymentSpec =
+                yamlMapper.readTree(classPathResource.getInputStream().readAllBytes());
+        return deploymentSpec;
+    }
 
-    deploymentSpec = getDeploymentSpec("v8-executive-1.0.0/deployment.yaml");
-    endpointObject = deploymentSpec.get("endpoints").get("/process");
-    Executor executor = adapter.activate("v8-executive-1.0.0", "", "", endpointObject);
-    Object helloResult = executor.execute("{\"name\":\"Bob\", \"weight\":70, \"height\":1.70}");
-    assertEquals("{message: \"Hello, Bob\", bmi: \"24.2\"}",
-            helloResult.toString()
-    );
-  }
 
 }
 
