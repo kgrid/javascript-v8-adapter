3ed591408b639ae4438d524b31596c7a756112ad
ngittlen <nggittle@med.umich.edu>
2021-02-08 13:42:14 -0500

    Updated tests to use junit 5

--- src/test/java/org/kgrid/adapter/v8/JsV8AdapterTest.java
+++ src/test/java/org/kgrid/adapter/v8/JsV8AdapterTest.java
@@ -2,112 +2,112 @@ package org.kgrid.adapter.v8;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.kgrid.adapter.api.ActivationContext;
 import org.kgrid.adapter.api.Adapter;
+import org.kgrid.adapter.api.AdapterException;
 import org.kgrid.adapter.api.Executor;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.net.URI;
-import java.nio.file.Paths;
 import java.util.Collections;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class JsV8AdapterTest {
 
-    Adapter adapter;
-    ObjectNode deploymentSpec;
+  Adapter adapter;
+  ObjectNode deploymentSpec;
 
-    @Mock
-    ActivationContext activationContext;
+  @Mock ActivationContext activationContext;
 
-    @Before
-    public void setUp() {
-        adapter = new JsV8Adapter();
-        adapter.initialize(activationContext);
-        deploymentSpec = new ObjectMapper().createObjectNode();
-        deploymentSpec.put("function", "hello");
-        deploymentSpec.put("artifact", "src/welcome.js");
+  @BeforeEach
+  public void setUp() {
+    adapter = new JsV8Adapter();
+    adapter.initialize(activationContext);
+    deploymentSpec = new ObjectMapper().createObjectNode();
+    deploymentSpec.put("function", "hello");
+    deploymentSpec.put("artifact", "src/welcome.js");
+  }
+
+  @Test
+  public void statusIsDownNoEngine() {
+    adapter = new JsV8Adapter();
+    assertEquals("DOWN", adapter.status());
+  }
+
+  @Test
+  public void statusIsUpWhenInitializedWithActivationContext() {
+    adapter.initialize(
+        new ActivationContext() {
+          @Override
+          public Executor getExecutor(String key) {
+            return null;
+          }
+
+          @Override
+          public InputStream getBinary(URI pathToBinary) {
+            return null;
+          }
+
+          @Override
+          public String getProperty(String key) {
+            return null;
+          }
+        });
+    assertEquals("UP", adapter.status());
+  }
+
+  @Test
+  public void badArtifactThrowsGoodError() {
+    RuntimeException runtimeException =
+        new RuntimeException("Binary resource not found src/tolkien.js");
+    when(activationContext.getBinary(URI.create("hello-world/src/tolkien.js")))
+        .thenThrow(runtimeException);
+    deploymentSpec.put("artifact", "src/tolkien.js");
+
+    Exception ex =
+        assertThrows(
+            AdapterException.class,
+            () -> adapter.activate(URI.create("hello-world/"), null, deploymentSpec));
+    assertAll(
+        () ->
+            assertEquals(
+                "Error loading source. Binary resource not found src/tolkien.js", ex.getMessage()),
+        () -> assertEquals(runtimeException, ex.getCause()));
+  }
+
+  @Test
+  public void throwsGoodErrorWhenActivateCantFindFunction() {
+    deploymentSpec.put("function", "goodbye1");
+    when(activationContext.getBinary(any(URI.class)))
+        .thenReturn(
+            new ByteArrayInputStream(
+                "function goodbye(name){ return 'Goodbye, ' + name;}".getBytes()));
+    try {
+      adapter.activate(URI.create("hello-world/"), null, deploymentSpec);
+    } catch (Exception ex) {
+      assertEquals("Error loading source", ex.getMessage());
+      assertEquals("Function goodbye1 not found", ex.getCause().getMessage());
     }
+  }
 
-    @Test
-    public void statusIsDownNoEngine() {
-        adapter = new JsV8Adapter();
-        assertEquals("DOWN", adapter.status());
-    }
+  @Test
+  public void worksWithListOfArtifacts() {
 
-    @Test
-    public void statusIsUpWhenInitializedWithActivationContext() {
-        adapter.initialize(
-                new ActivationContext() {
-                    @Override
-                    public Executor getExecutor(String key) {
-                        return null;
-                    }
+    deploymentSpec.set("artifact", new ObjectMapper().createArrayNode().add("src/tolkien.js"));
+  }
 
-                    @Override
-                    public InputStream getBinary(URI pathToBinary) {
-                        return null;
-                    }
-
-                    @Override
-                    public String getProperty(String key) {
-                        return null;
-                    }
-                });
-        assertEquals("UP", adapter.status());
-    }
-
-    @Test
-    public void badArtifactThrowsGoodError() {
-        RuntimeException runtimeException =
-                new RuntimeException("Binary resource not found src/tolkien.js");
-        when(activationContext.getBinary(URI.create("hello-world/src/tolkien.js")))
-                .thenThrow(runtimeException);
-        deploymentSpec.put("artifact", "src/tolkien.js");
-
-        try {
-            adapter.activate(URI.create("hello-world/"), null, deploymentSpec);
-        } catch (Exception ex) {
-            assertEquals("Error loading source. Binary resource not found src/tolkien.js", ex.getMessage());
-            assertEquals(runtimeException, ex.getCause());
-        }
-    }
-
-    @Test
-    public void throwsGoodErrorWhenActivateCantFindFunction() throws Exception {
-        deploymentSpec.put("function", "goodbye1");
-        when(activationContext.getBinary(any(URI.class)))
-                .thenReturn(new ByteArrayInputStream("function goodbye(name){ return 'Goodbye, ' + name;}".getBytes()));
-        try {
-            adapter.activate(URI.create("hello-world/"), null, deploymentSpec);
-        } catch (Exception ex) {
-            assertEquals("Error loading source", ex.getMessage());
-            assertEquals("Function goodbye1 not found", ex.getCause().getMessage());
-        }
-    }
-
-    @Test
-    public void worksWithListOfArtifacts() {
-
-        deploymentSpec.set("artifact", new ObjectMapper().createArrayNode().add("src/tolkien.js"));
-
-    }
-
-    @Test
-    public void getTypeReturnsV8() {
-        assertEquals(Collections.singletonList("javascript"), adapter.getEngines());
-    }
-
+  @Test
+  public void getTypeReturnsV8() {
+    assertEquals(Collections.singletonList("javascript"), adapter.getEngines());
+  }
 }
