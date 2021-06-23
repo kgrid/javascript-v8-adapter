1fe1e3dcde59e30fd1d89d3aad390e7a3f6cc279
ngittlen <nggittle@med.umich.edu>
2021-06-23 15:12:26 -0400

    Removed create sharable as the system works without it.

--- src/main/java/org/kgrid/adapter/v8/V8Executor.java
+++ src/main/java/org/kgrid/adapter/v8/V8Executor.java
@@ -2,8 +2,6 @@ package org.kgrid.adapter.v8;
 
 import org.graalvm.polyglot.PolyglotException;
 import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.proxy.ProxyArray;
-import org.graalvm.polyglot.proxy.ProxyObject;
 import org.kgrid.adapter.api.AdapterClientErrorException;
 import org.kgrid.adapter.api.AdapterException;
 import org.kgrid.adapter.api.AdapterServerErrorException;
@@ -20,7 +18,6 @@ public class V8Executor implements Executor {
     @Override
     public synchronized Object execute(Object input, String contentType) {
         try {
-            input = createSharable(Value.asValue(input));
             Value result = function.execute(input);
             return result.as(Object.class);
         } catch (PolyglotException e) {
@@ -33,73 +30,4 @@ public class V8Executor implements Executor {
             throw new AdapterException(e.getMessage(), e);
         }
     }
-
-    // Method to de-contextualize a value which may be passed between objects
-    // Remove when https://github.com/oracle/graal/issues/631#issuecomment-716611797 is fixed
-    private static Object createSharable(Value v) {
-        if (v.isBoolean()) {
-            return v.asBoolean();
-        } else if (v.isNumber()) {
-            return v.as(Number.class);
-        } else if (v.isString()) {
-            return v.asString();
-        } else if (v.isHostObject()) {
-            return v.asHostObject();
-        } else if (v.isProxyObject()) {
-            return v.asProxyObject();
-        } else if (v.hasArrayElements()) {
-            return new ProxyArray() {
-                @Override
-                public void set(long index, Value value) {
-                    v.setArrayElement(index, createSharable(value));
-                }
-
-                @Override
-                public long getSize() {
-                    return v.getArraySize();
-                }
-
-                @Override
-                public Object get(long index) {
-                    return createSharable(v.getArrayElement(index));
-                }
-
-                @Override
-                public boolean remove(long index) {
-                    return v.removeArrayElement(index);
-                }
-            };
-        } else if (v.hasMembers()) {
-            return new ProxyObject() {
-
-                @Override
-                public void putMember(String key, Value value) {
-                    v.putMember(key, createSharable(value));
-                }
-
-                @Override
-                public boolean hasMember(String key) {
-                    return v.hasMember(key);
-                }
-
-                @Override
-                public Object getMemberKeys() {
-                    return v.getMemberKeys().toArray(new String[0]);
-                }
-
-                @Override
-                public Object getMember(String key) {
-                    return createSharable(v.getMember(key));
-                }
-
-                @Override
-                public boolean removeMember(String key) {
-                    return v.removeMember(key);
-                }
-            };
-        } else {
-            throw new UnsupportedOperationException("Unsupported value");
-        }
-    }
-
 }
